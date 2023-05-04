195814333c0476474bc9b5b15f41dc2c71339cbf
Noah Saso <noahsaso@gmail.com>
2023-05-04 11:06:19 -0700

    Upgraded cosmodal.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,23 +19,30 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <WalletManagerProvider
-        classNames={{
-          modalOverlay: '!backdrop-brightness-50 !backdrop-filter',
-          modalContent:
-            '!p-6 !max-w-md !bg-background-base !rounded-lg !border !border-border-secondary !shadow-dp8',
-          modalCloseButton:
-            '!p-1 !text-icon-tertiary bg-transparent hover:!bg-background-interactive-hover active:!bg-background-interactive-pressed !rounded-full !transition !absolute !top-2 !right-2',
-          modalHeader: '!header-text',
-          modalSubheader: '!title-text',
-          wallet:
-            '!rounded-lg !bg-background-secondary !p-4 !shadow-none transition-opacity opacity-100 hover:opacity-80 active:opacity-70',
-          walletImage: '!rounded-full',
-          walletName: '!primary-text',
-          walletDescription: '!caption-text',
-          textContent: '!body-text',
-        }}
         defaultChainId={ChainInfoID.Juno1}
-        enabledWalletTypes={[WalletType.Keplr, WalletType.WalletConnectKeplr]}
+        defaultUiConfig={{
+          classNames: {
+            modalOverlay: '!backdrop-brightness-50 !backdrop-filter',
+            modalContent:
+              '!p-6 !max-w-md !bg-background-base !rounded-lg !border !border-border-secondary !shadow-dp8',
+            modalCloseButton:
+              '!p-1 !text-icon-tertiary bg-transparent hover:!bg-background-interactive-hover active:!bg-background-interactive-pressed !rounded-full !transition !absolute !top-2 !right-2',
+            modalHeader: '!header-text',
+            modalSubheader: '!title-text',
+            wallet:
+              '!rounded-lg !bg-background-secondary !p-4 !shadow-none transition-opacity opacity-100 hover:opacity-80 active:opacity-70',
+            walletImage: '!rounded-full',
+            walletName: '!primary-text',
+            walletDescription: '!caption-text',
+            textContent: '!body-text',
+          },
+          renderLoader: () => <Loader className="mt-2" fill size={32} />,
+        }}
+        enabledWalletTypes={[
+          WalletType.Keplr,
+          WalletType.KeplrMobile,
+          WalletType.Leap,
+        ]}
         getSigningCosmWasmClientOptions={(chainInfo) => ({
           gasPrice: GasPrice.fromString(
             '0.0025' + chainInfo.feeCurrencies[0].coinMinimalDenom
@@ -47,7 +54,6 @@ export default function App({ Component, pageProps }: AppProps) {
           ),
         })}
         localStorageKey="connectedWallet"
-        renderLoader={() => <Loader className="mt-2" fill size={32} />}
         walletConnectClientMeta={{
           name: 'CosmWasm Indexer',
           description: 'Indexer for CosmWasm smart contracts.',
